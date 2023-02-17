ba7d4ad05fc6840849181e0079e9a42b9f9d7223
yeaahYEAHH <81806907+yeaahYEAHH@users.noreply.github.com>
2023-02-17 15:33:51 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 
 $(".dws-form").on("click", ".tab", function() {
     // Удаляем классы active
-    $(".dws-form .tab").removeClass('active');
- 
+    $(".dws-form .tab-form").removeClass('active');
+    $(".dws-form .tab").removeClass('activate')
+
     // Добавляем классы active
     $(this).addClass("active");
+
     $(".tab-form").eq($(this).index()).addClass("active");
+    $(".tab").eq($(this).index()).addClass("activate");
 });
